de7f89c724281178a42e1cebcaeac4316eb82125
RiversCoder <928091232@qq.com>
2018-09-24 20:36:12 +0800

    new

---
+++ README.md
@@ -0,0 +1,26 @@
+## 介绍
+
+`jk-ui` 是一款基于 `Vue.js 2.0` 的前端 `UI`组件库, 其中文件夹`package`为`jk-ui`源码
+整个项目为`jk-ui`的API
+
+## 在线演示
+
+[演示文档](http://www.goingtrace.com)
+
+## 特性
+
+- 基于 `Vue` 开发的 `UI` 组件
+- 使用 `npm + webpack + babel` 的工作流，支持 ES6
+- 提供友好的 API，可灵活的使用组件
+
+## 启动
+>1. npm intall
+>2. npm run dev
+
+## 使用jk-ui
+>1. `npm install --save jk-ui`
+>2. 在Vue项目里的`main.js`
+>>`import JKUI from 'jk-ui'`<br/>
+>>`import 'jk-ui/theme-default/lib/index.css'`<br/>
+>>`Vue.use(JKUI)`
+
---
+++ packages/index.js
@@ -0,0 +1,45 @@
+/*
+* @author jike
+* Date: 18/7/27
+*/
+import Vue from 'vue'
+import JButton from './button/index'
+import JCol from './col/index'
+import JRow from './row/index'
+import JTag from './tag/index'
+import Message from './message/index'
+import JBreadcrumb from './breadcrumb/index'
+import JBreadcrumbItem from './breadcrumb-item/index'
+import JCard from './card/index'
+import towTable from './two-dimensional-table/index';
+
+const components = [
+  JButton,
+  JCol,
+  JRow,
+  JTag,
+  JBreadcrumb,
+  JBreadcrumbItem,
+  JCard,
+  towTable
+]
+
+
+const install = function(Vue) {
+  if(install.installed) return
+  components.map(component => Vue.component(component.name, component))
+  Vue.prototype.$message = Message
+}
+
+
+export default {
+  install,
+  JButton,
+  JCol,
+  JRow,
+  JTag,
+  JBreadcrumb,
+  JBreadcrumbItem,
+  JCard,
+  towTable
+}
